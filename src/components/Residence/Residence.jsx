83b6e02d4aa5e54a80759fcd5dfb1f2c7c5b7437
Skaix <167339725+skaixxx@users.noreply.github.com>
2025-06-07 15:20:24 +0000

    changes

--- src/components/Residence/Residence.jsx
+++ src/components/Residence/Residence.jsx
@@ -111,10 +111,10 @@ function Residence() {
                 <p>КАК ИМ СТАТЬ? ЗАПОЛНИ ЗАЯВКУ!</p>
             </div>
             <div className="bid-form">
-                <div className="form-name"><input className="input-r" type="name" id="name" placeholder='фио' data-placeholder='фио' onFocus={inputFocus} onBlur={inputBlur}></input><img className="profile-icon" src={profileIcon} alt="profile"/></div>
-                <div className="form-birthday"><input className="input-r" type="text" name="birthday" id="birthday" placeholder="дата рождения" data-placeholder='дата рождения' onFocus={inputFocus} onBlur={inputBlur}></input><img className="calendar-icon" src={calendarIcon} alt="calendar"></img></div>
-                <div className="form-phone"><input className="input-r" type="tel" name="phone" id="phone" placeholder="телефон" data-placeholder='телефон' onFocus={inputFocus} onBlur={inputBlur}></input><img className="phone-icon" src={phoneIcon} alt="phone"/></div>
-                <div className="form-email"><input className="input-r" type="email" name="email" id="email" placeholder="email" data-placeholder='email' onFocus={inputFocus} onBlur={inputBlur}></input><img className="email-icon" src={emailIcon} alt="email"/></div>
+                <div className="form-name"><input className="input-r" type="name" id="name" placeholder='фио' data-placeholder='фио' onFocus={inputFocus} onBlur={inputBlur}/><img className="profile-icon" src={profileIcon} alt="profile"/></div>
+                <div className="form-birthday"><input className="input-r" type="text" name="birthday" id="birthday" placeholder="дата рождения" data-placeholder='дата рождения' onFocus={inputFocus} onBlur={inputBlur}/><img className="calendar-icon" src={calendarIcon} alt="calendar"></img></div>
+                <div className="form-phone"><input className="input-r" type="tel" name="phone" id="phone" placeholder="телефон" data-placeholder='телефон' onFocus={inputFocus} onBlur={inputBlur}/><img className="phone-icon" src={phoneIcon} alt="phone"/></div>
+                <div className="form-email"><input className="input-r" type="email" name="email" id="email" placeholder="email" data-placeholder='email' onFocus={inputFocus} onBlur={inputBlur}/><img className="email-icon" src={emailIcon} alt="email"/></div>
                 <div className="form-send-button"><ButtonGoToDesktop text="отправить"/></div>
             </div>
             <div className="bid-picture-container"><img src="./images/residence/residenceBottom.png" alt="bottomPhoto" className="bid-picture"></img></div>
