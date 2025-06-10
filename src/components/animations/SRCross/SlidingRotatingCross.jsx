b6e54063b5a2df7d85875732162e5a5e551c04fd
Skaix <167339725+skaixxx@users.noreply.github.com>
2025-06-10 18:25:11 +0000

    update

--- src/components/animations/SRCross/SlidingRotatingCross.jsx
+++ src/components/animations/SRCross/SlidingRotatingCross.jsx
@@ -7,9 +7,9 @@ import cross from "../../../assets/animation/1/black_cross.svg"
 function SlidingRotatingCross() {
     return (
         <div className="sr-animation-container">
-            <div className="b_dot"><img className="sr-anim-element" src={b_dot} alt="b_dot"/></div>
-            <div className="r_dot"><img className="sr-anim-element" src={r_dot} alt="b_dot"/></div>
-            <div className="cross"><img className="sr-anim-element" src={cross} alt="b_dot"/></div>
+            <div className="b_dot"><img className="sr-anim-element1" src={b_dot} alt="b_dot"/></div>
+            <div className="r_dot"><img className="sr-anim-element1" src={r_dot} alt="b_dot"/></div>
+            <div className="cross"><img className="sr-anim-element2" src={cross} alt="b_dot"/></div>
         </div>
     );
 }
