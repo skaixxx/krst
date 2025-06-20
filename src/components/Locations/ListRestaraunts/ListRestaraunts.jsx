836a8de9f066fcfe0adc66453a2845d68f3fa023
Maks75g <savostin-maksim@inbox.ru>
2025-06-20 18:23:16 +0300

    Добавлена и разрабатывается страница "СпискиЛок"
    Добавлена и разрабатывается страница "СпискиЛок"

---
+++ src/components/Locations/ListRestaraunts/ListRestaraunts.jsx
@@ -0,0 +1,34 @@
+import ListRestaraunt1 from "../../../assets/locations/ListRestaraunt1.png";
+import ListRestaraunt2 from "../../../assets/locations/ListRestaraunt2.png";
+import ListRestaraunt3 from "../../../assets/locations/ListRestaraunt3.png";
+import ListRestaraunt4 from "../../../assets/locations/ListRestaraunt4.png";
+import ListRestaraunt5 from "../../../assets/locations/ListRestaraunt5.png";
+import ListRestaraunt6 from "../../../assets/locations/ListRestaraunt6.png";
+import ListRestaraunt7 from "../../../assets/locations/ListRestaraunt7.png";
+import "./ListRestaraunts.css";
+import Dots from "../../../components/animations/ScaleSlideDots/ScaleSlideDots";
+import RotatingCross from "../../../components/animations/RotatingCross/RotatingCross";
+
+
+function ListRestaraunt () {
+
+    return (
+
+    <div className="ListRestarauntPage">
+        <div className="ListRestarauntContainer">
+            <div className="ListRestarauntTitle">
+    <p className="LRTitle-1">
+        СПИСОК 
+    </p>
+            <div className="ListRestarauntDots">
+        <Dots/>
+            </div>
+            </div>
+    
+
+</div>
+</div>
+
+    );
+}
+export default ListRestaraunt;
