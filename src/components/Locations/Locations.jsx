8d73e5acb757cdbdff21b3dd6608cd2a8d0cb1b4
mr_yes_dev <mryes.develop@gmail.com>
2025-06-23 23:59:30 +0300

    Created the photos feed component

--- src/components/Locations/Locations.jsx
+++ src/components/Locations/Locations.jsx
@@ -1,49 +1,17 @@
-import "./Locations.css"
-import "../general.css";
-import { useNavigate } from "react-router";
-import { useEffect } from "react";
+import PhotoGallery from "./PhotoGallery";
 
+const Locations = () => {
 
-function Locations() {
-    const navigate = useNavigate();
-    useEffect(() => {
-        navigate(`/Locations/restraunts`)
-    }, [navigate]);
-           
-    return null;
+    const galleryPhotos = [
+        {url: './images/locations/restaurants.png', alt: 'Рестораны'},
+        {url: './images/locations/masterClasses.png', alt: 'Мастер-классы'},
+        {url: './images/locations/excursions.png', alt: 'Экскурсии'}
+    ];
+    
+    return(
+        <PhotoGallery
+            photos={galleryPhotos}/>
+    )  
 }
+
 export default Locations;
-
-
-// function Locations() {
-//     const { items } = useData();
-//         const navigate = useNavigate();
-//         const handleClick = (id) => {
-//                 navigate(`/Locations/SpecificLocations/${id}`)
-//         }
-    //  console.log("Data: ", items);
-    // if (!items || items.length === 0) {
-    //     return <p>Данные загружаются...</p>
-    // };
-    // return (
-//     <div className="grid-container-locations">
-//     {items.map((item) =>(
-//     <a className={`locationsCard${item.id}`} key={item.id} onClick={() => handleClick(item.id)} style={{
-//         backgroundImage: `url(${item.image})`,
-//         backgroundPosition: "center",
-//         backgroundSize: "cover",
-//         //width: `23,5 rem`,
-//         //height: `17,5 rem`,
-//         cursor: "pointer"
-//     }}> 
-//     </a>
-
-    
-//         ))}
-
-//     </div>
-    
-         
-//              );
-// }
-// export default Locations;
