dc888dc2b5e9269c6f04458438ab28788cc2ae03
mr_yes_dev <mryes.develop@gmail.com>
2025-06-25 22:09:50 +0300

    Created a mobile version of the photo carousel

--- src/components/PhotoCards/PhotoGallery.jsx
+++ src/components/PhotoCards/PhotoGallery.jsx
@@ -21,12 +21,55 @@ const PhotoGallery = ({photos = [], imageWidth = "18.8rem", imageHeight = "14rem
             height: imageHeight
         };
     }
+
+    const calculatePosition = (index) =>  {
+        if (window.innerWidth <= 768){
+            
+            const totalPhotos = photos.length;
+            let offset = index - selectedIndex;
+
+            // Зацикливание для первого и последнего элемента
+            if (selectedIndex === 0 && index === totalPhotos - 1){
+                offset = -1;
+            } else if (selectedIndex === totalPhotos - 1 && index === 0){
+                offset = 1;
+            }
+            
+            switch(offset) {
+                case 0:
+                    return {
+                        transform: "translateX(0)",
+                        zIndex: "2"
+                    };
+                case -1:
+                    return {
+                        transform: "translateX(-50%)",
+                        zIndex: "1"
+                    };
+                case 1:
+                    return {
+                        transform: "translateX(50%)",
+                        zIndex: "1"
+                    };
+                default:
+                    return {
+                        display: "none"
+                    };
+            }
+        }
+        return {};
+    }
     
     return(
         <div className={style.gallery} style={{ height: `calc(${imageHeight} * 1.1)` }}>
         {
             photos.map((photo, index) => (
-                <div key={index} onClick={() => handleImageClick(index)}>
+                <div
+                    key={index}
+                    onClick={() => handleImageClick(index)}
+                    className={style.container}
+                    style={calculatePosition(index)}    
+                    >
                     <img
                         src={photo.url}
                         alt={photo.alt || `Фото ${index + 1}`}
