f0a20104a549128cd071675908c1e04a9cb1a6eb
mr_yes_dev <mryes.develop@gmail.com>
2025-07-06 19:05:35 +0300

    Added animation to the text on the history page

--- src/components/History/PersonCards.jsx
+++ src/components/History/PersonCards.jsx
@@ -1,25 +1,43 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import styles from "./PersonCardsStyle.module.css";
 import PhotoGallery from "../PhotoCards/PhotoGallery";
 
 export default function CarouselContent({data}) {
-  const [activeIndex, setActiveIndex] = useState(0);
-if (data.items.length === 0) return <div>Загрузка...</div>;
+  
+  // Состояние для хранения выбранного индекса
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [displayedIndex, setDisplayedIndex] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false); 
+
+  useEffect(() => {
+      if (selectedIndex !== displayedIndex) {
+          setIsAnimating(true) // Запускаем анимацию
+          const timer = setTimeout(() => {
+              setDisplayedIndex(selectedIndex);
+              setIsAnimating(false);
+          }, 300);
+      
+          return () => clearTimeout(timer);
+      }
+  }, [selectedIndex, displayedIndex]);
+
+  if (data.items.length === 0) return <div>Загрузка...</div>;
+  
   return (
     <div className={styles.wrapper}>
       <PhotoGallery 
         photos={data.items}
-        onPhotoSelect={setActiveIndex}
+        onPhotoSelect={setSelectedIndex}
         isMonochrome={true}
         desktopSize={data.desktopSize}
       />
       <div className={styles.underPhotoContent}>
         <div className={styles.column1}>
-          <p className={styles.cardYear}>{data.items[activeIndex].year}</p>
-          <p className={styles.cardTitle}>{data.items[activeIndex].name}</p>
+          <p className={`${styles.cardYear} ${isAnimating ? styles.fadeOut : styles.fadeIn}`}>{data.items[displayedIndex].year}</p>
+          <p className={`${styles.cardTitle} ${isAnimating ? styles.fadeOut : styles.fadeIn}`}>{data.items[displayedIndex].name}</p>
         </div>
         <div className={styles.column2}>
-          <p className={styles.cardParagraph}>{data.items[activeIndex].description}</p>
+          <p className={`${styles.cardParagraph} ${isAnimating ? styles.fadeOut : styles.fadeIn}`}>{data.items[displayedIndex].description}</p>
         </div>
       </div>
     </div>
