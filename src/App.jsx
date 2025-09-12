bf85650a60f75499dc8eb6c22df823eb87c130e4
skaix <iamlefortoviy@gmail.com>
2025-09-12 18:30:29 +0300

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, HashRouter as Router} from 'react-router';
+import { HashRouter as Router} from 'react-router';
 import Footer from './components/Footer/footer';
 import Header from './components/Header/Header'
 import AppRoutes from './routes/routes';
@@ -9,14 +9,14 @@ import PopUp from './components/popup/popup';
 function App() {
   return (
     <React.StrictMode>
-      <BrowserRouter>
+      <Router>
         <PopupProvider>
             <Header/>
               <AppRoutes/>
             <Footer/>
             <PopUp/>
         </PopupProvider>
-      </BrowserRouter>
+      </Router>
     </React.StrictMode>
   );
 }
